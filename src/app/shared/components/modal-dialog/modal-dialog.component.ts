392197c7c46510f51e3fdbbcdc130d4cbe84a22a
ksalayon <switchashanti@gmail.com>
2025-04-06 00:32:48 +1300

    Feat: Initial setup for reusable modal dialog component

--- src/app/shared/components/modal-dialog/modal-dialog.component.ts
+++ src/app/shared/components/modal-dialog/modal-dialog.component.ts
@@ -1,13 +1,29 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+// modal-dialog.component.ts
+import { Component, Injector, Type, ViewChild } from '@angular/core';
+import { CdkPortalOutlet, ComponentPortal, PortalModule } from '@angular/cdk/portal';
 
 @Component({
+  selector: 'phq-modal',
   standalone: true,
-  selector: 'phq-modal-dialog',
-  imports: [],
-  templateUrl: './modal-dialog.component.html',
-  styleUrl: './modal-dialog.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [PortalModule],
+  template: `
+    <div class="modal-backdrop" (click)="close()"></div>
+    <div class="modal-content">
+      <ng-template cdkPortalOutlet></ng-template>
+    </div>
+  `,
+  styleUrls: ['./modal-dialog.component.scss'],
 })
 export class ModalDialogComponent {
-  public title?: string;
+  @ViewChild(CdkPortalOutlet, { static: true }) outlet!: CdkPortalOutlet;
+  onClose!: () => void;
+
+  close() {
+    this.onClose?.();
+  }
+
+  attachComponent<T>(component: Type<T>, injector: Injector) {
+    const portal = new ComponentPortal(component, null, injector);
+    this.outlet.attachComponentPortal(portal);
+  }
 }
---
+++ src/app/shared/services/modal-dialog.service.ts
@@ -0,0 +1,70 @@
+import {
+  ApplicationRef,
+  ComponentRef,
+  EnvironmentInjector,
+  EventEmitter,
+  inject,
+  Injectable,
+  Injector,
+  Type,
+} from '@angular/core';
+import { ModalDialogComponent } from '../components/modal-dialog/modal-dialog.component';
+
+type OutputHandlers<T> = {
+  [K in keyof T]?: T[K] extends EventEmitter<infer U> ? (event: U) => void : never;
+};
+
+@Injectable({ providedIn: 'root' })
+export class ModalService {
+  private appRef = inject(ApplicationRef);
+  private envInjector = inject(EnvironmentInjector);
+  private modalRef: ComponentRef<ModalDialogComponent> | null = null;
+
+  open<T>(
+    component: Type<T>,
+    options?: {
+      inputs?: Partial<T>;
+      outputs?: OutputHandlers<T>;
+    }
+  ) {
+    const modalEl = document.createElement('phq-modal');
+    document.body.appendChild(modalEl);
+
+    const compRef = this.appRef.bootstrap(ModalDialogComponent, modalEl);
+    compRef.instance.onClose = () => this.close();
+
+    const injector = this.createInjector(options?.inputs);
+    compRef.instance.attachComponent(component, injector);
+
+    // Handle outputs safely
+    const attachedRef = compRef.instance.outlet.attachedRef;
+    if (attachedRef && 'instance' in attachedRef) {
+      const embeddedComp = attachedRef.instance as T;
+      for (const [key, handler] of Object.entries(options?.outputs ?? {})) {
+        const output = (embeddedComp as any)[key];
+        if (output?.subscribe) {
+          output.subscribe(handler);
+        }
+      }
+    }
+
+    this.modalRef = compRef;
+  }
+
+  private createInjector<T>(inputs?: Partial<T>): Injector {
+    return Injector.create({
+      providers: Object.entries(inputs || {}).map(([key, value]) => ({
+        provide: key,
+        useValue: value,
+      })),
+      parent: this.envInjector,
+    });
+  }
+
+  close() {
+    if (this.modalRef) {
+      this.modalRef.destroy();
+      this.modalRef = null;
+    }
+  }
+}
