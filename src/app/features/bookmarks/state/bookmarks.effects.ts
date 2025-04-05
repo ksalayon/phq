07dc96c217acd9e92d36244a8a52d5c3612e8e48
ksalayon <switchashanti@gmail.com>
2025-04-05 22:20:23 +1300

    Handle check for duplicate url entry in effects

--- src/app/features/bookmarks/state/bookmarks.effects.ts
+++ src/app/features/bookmarks/state/bookmarks.effects.ts
@@ -1,30 +1,50 @@
 import { inject, Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { catchError, map, mergeMap } from 'rxjs/operators';
+import { catchError, map, mergeMap, take } from 'rxjs/operators';
 import { BookmarksActions } from './bookmarks.actions';
 import { BookmarkService } from '../services/bookmark.service';
 import { of } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { selectAllBookmarks } from './bookmarks.selectors';
 
 @Injectable()
 export class BookmarksEffects {
   private actions$ = inject(Actions);
   private bookmarkService = inject(BookmarkService);
+  private store = inject(Store);
 
   createBookmark$ = createEffect(() =>
     this.actions$.pipe(
       ofType(BookmarksActions.createBookmark),
-      mergeMap((action) => {
-        return this.bookmarkService.createBookmark(action.payload).pipe(
-          map((bookmark) => {
-            return BookmarksActions.createBookmarkSuccess({ bookmark });
-          }),
-          catchError((error) => {
-            return of(
-              BookmarksActions.createBookmarkFailure(error?.message || 'Bookmark creation failed')
+      mergeMap((action) =>
+        this.store.select(selectAllBookmarks).pipe(
+          take(1), // ensure it completes
+          mergeMap((bookmarks) => {
+            const bookmarkExists = bookmarks.some(
+              (bookmark) => bookmark.name === action.payload.name
+            );
+
+            if (bookmarkExists) {
+              return of(
+                BookmarksActions.createBookmarkFailure({
+                  error: 'Bookmark with the same name already exists',
+                })
+              );
+            }
+
+            return this.bookmarkService.createBookmark(action.payload).pipe(
+              map((bookmark) => BookmarksActions.createBookmarkSuccess({ bookmark })),
+              catchError((error) =>
+                of(
+                  BookmarksActions.createBookmarkFailure({
+                    error: error?.message || 'Bookmark creation failed',
+                  })
+                )
+              )
             );
           })
-        );
-      })
+        )
+      )
     )
   );
 }
