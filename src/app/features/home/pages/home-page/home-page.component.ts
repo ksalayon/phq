882f34470c14591fa160bdd57b51f1c6fe14fa15
ksalayon <switchashanti@gmail.com>
2025-04-09 01:50:33 +1200

    Deleted now extraneous home page component

--- src/app/features/home/pages/home-page/home-page.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'phq-home-page',
-  imports: [],
-  templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.css',
-})
-export class HomePageComponent {}
